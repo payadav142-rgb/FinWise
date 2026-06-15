f217c0a45946dec3f13e56128b9e2d6e8d0a3669
Pawan <payadav142@gmail.com>
2026-06-15 17:42:55 +0530

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,11 +25,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.5,
+      priority: 0.6,
     },
 
     {
       url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/terms-and-conditions`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/disclaimer`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
