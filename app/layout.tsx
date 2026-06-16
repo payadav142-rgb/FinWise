06138435b63a46b805cb36b2d4659e37aa24ede7
Pawan <payadav142@gmail.com>
2026-06-16 19:29:42 +0530

    Added Google Search Console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,13 @@ default: "FinWise - Financial Calculators for India",
 template: "%s | FinWise",
 },
 description:
-"Free financial calculators for SIP, EMI, FD, RD, CAGR, Tax and more.",
+"Free financial calculators for SIP, EMI, FD, RD, CAGR, Income Tax and more.",
+verification: {
+  google: "c2E0Y9rYTzrEi_aYV43C725aFKUMCcaoyREP1or5amo",
+},
+
 };
+
 
 export default function RootLayout({
 children,
@@ -31,19 +36,27 @@ return (
 <html
 lang="en"
 className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-> <body className="min-h-full flex flex-col">
+> 
+<body className="min-h-full flex flex-col">
 {children}
 
-
+    {/* Google Analytics */}
     <Script
       src="https://www.googletagmanager.com/gtag/js?id=G-2345F2VRYG"
       strategy="afterInteractive"
     />
 
-    <Script id="google-analytics" strategy="afterInteractive">
+    <Script
+      id="google-analytics"
+      strategy="afterInteractive"
+    >
       {`
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'G-2345F2VRYG');
