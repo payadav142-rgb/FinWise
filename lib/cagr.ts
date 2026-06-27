4960b4fc101fd45d68fa0646795c993ee50cf331
Pawan <payadav142@gmail.com>
2026-06-27 18:53:58 +0530

    Add validation to CAGR calculator

--- lib/cagr.ts
+++ lib/cagr.ts
@@ -3,6 +3,17 @@ export function calculateCAGR(
   endingValue: number,
   years: number
 ) {
+  // Input Validation
+  if (
+    beginningValue <= 0 ||
+    endingValue <= 0 ||
+    years <= 0
+  ) {
+    return {
+      cagr: 0,
+    };
+  }
+
   const cagr =
     (Math.pow(
       endingValue / beginningValue,
