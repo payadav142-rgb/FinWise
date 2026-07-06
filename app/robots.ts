f4ac8b5e8436dcdf179607a02b4977eacb3429d8
Pawan <payadav142@gmail.com>
2026-07-06 20:17:18 +0530

    refactor: simplify robots.txt configuration

--- app/robots.ts
+++ app/robots.ts
@@ -4,22 +4,12 @@ const BASE_URL = "https://finwise-silk.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-
-        disallow: [
-          "/api/",
-          "/_next/",
-          "/404",
-          "/500",
-        ],
-      },
-    ],
-
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/_next/"],
+    },
     sitemap: `${BASE_URL}/sitemap.xml`,
-
     host: BASE_URL,
   };
 }
